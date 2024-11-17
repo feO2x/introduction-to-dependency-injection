542faf8edf67927ebce7f9c0c0e413adf12d72f9
Kenny Pflug <kenny.pflug@live.de>
2024-11-17 10:17:26 +0100

    feat: add initial console app with copy to console functionality
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Copy();
+    }
+
+    private static void Copy()
+    {
+        while (true)
+        {
+            var readKey = Console.ReadKey(intercept: true);
+            if (readKey.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            Console.Write(readKey.KeyChar);
+        }
+    }
+}
