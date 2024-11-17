7c4a79a95f19487018b1a1fbe5dac10e454935a5
Kenny Pflug <kenny.pflug@live.de>
2024-11-17 17:29:53 +0100

    feat: introduce File and Target
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,40 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
+using System.IO;
+
 
 namespace ConsoleApp;
+
+public static class File
+{
+    private static StreamWriter? streamWriter;
+
+    public static void Initialize(string path)
+    {
+        streamWriter = new StreamWriter(new FileStream(path, FileMode.Create, FileAccess.Write));
+    }
+
+    public static void Write(char value)
+    {
+        if (streamWriter is null)
+        {
+            throw new InvalidOperationException("File stream is not initialized.");
+        }
+
+        streamWriter.Write(value);
+    }
+    
+    public static void Dispose()
+    {
+        streamWriter?.Close();
+        streamWriter = null;
+    }
+}
+
+public enum Target
+{
+    Console,
+    File
+}
 
 public static class Program
 {
